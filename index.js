d0d2433d11d17f2f5063d7b37ba65611c43f08fb
Amira_Ezaat <76440009+Amiraezaat@users.noreply.github.com>
2022-07-30 16:40:11 +0200

    NodeCommit

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const express = require('express')
 const { connection_to_DB } = require('./DB/connection')
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT
 const User_Router = require('./Modules/User_module/User_Routes')
-const cors = require('cors')
-app.use(express.json())
+ app.use(express.json())
 app.use("/pictures" , express.static("./pictures"))
 
 // ======================== cors ========================
 app.use(
   cors({
-    credentials: true,
-    //   origin: ["http://192.168.1.18:4200"],
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization', 'origin', 'x-csrf-token'],
-    optionsSuccessStatus: 204,
-    preflightContinue: false
-  })
+    origin: '*'
+})
 )
 
 connection_to_DB()
